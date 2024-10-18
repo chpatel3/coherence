f405e885f1de814cc3a1256971364ef063ad8d20
chpatel <user@domain.net>
2024-10-18 10:04:42 -0400

    test

--- prj/coherence-core-components/src/main/java/com/tangosol/coherence/component/util/daemon/queueProcessor/service/grid/partitionedService/partitionedCache/Storage.java
+++ prj/coherence-core-components/src/main/java/com/tangosol/coherence/component/util/daemon/queueProcessor/service/grid/partitionedService/partitionedCache/Storage.java
@@ -12794,7 +12794,7 @@ public class Storage
             {
             // import com.tangosol.net.cache.CacheMap;
 
-            checkMutable();
+            // checkMutable();
 
             setExpiryValue(cMillis >= 0 ? cMillis : CacheMap.EXPIRY_NEVER);
 
